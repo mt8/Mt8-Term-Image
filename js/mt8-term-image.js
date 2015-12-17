bff4ebf206908376bd93ba1ccafa27b0d554cd42
mt8 <info@mt8.biz>
2015-12-17 17:35:33 +0900

    fix js indent

--- js/mt8-term-image.js
+++ js/mt8-term-image.js
@@ -1,25 +1,25 @@
 jQuery(document).ready(function($){
-    var custom_uploader;
-    $('#mt8-term-image-up').click(function(e) {
-        e.preventDefault();
-        if (custom_uploader) {
-            custom_uploader.open();
-            return;
-        } 
-        custom_uploader = wp.media({
-            library: {
-                type: 'image'
-            }, 
-            multiple: false
-        }); 
-        custom_uploader.on('select', function() {
-            var images = custom_uploader.state().get('selection');
+	var custom_uploader;
+	$('#mt8-term-image-up').click(function(e) {
+		e.preventDefault();
+		if (custom_uploader) {
+			custom_uploader.open();
+			return;
+		} 
+		custom_uploader = wp.media({
+			library: {
+				type: 'image'
+			}, 
+			multiple: false
+		}); 
+		custom_uploader.on('select', function() {
+			var images = custom_uploader.state().get('selection');
 			$('#mt8-term-image').html('');
-            images.each(function(file){
-                $('#mt8-term-image').append('<img src="'+file.toJSON().url+'" />');
-                $('#mt8-term-image-inp').val(file.toJSON().id);				
-            });
-        }); 
-        custom_uploader.open();
-    }); 
+			images.each(function(file){
+				$('#mt8-term-image').append('<img src="'+file.toJSON().url+'" />');
+				$('#mt8-term-image-inp').val(file.toJSON().id);				
+			});
+		}); 
+		custom_uploader.open();
+	}); 
 });
